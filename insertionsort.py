a0617d3284fc2e750abcc1efdeb00e9fc9082ff6
shashikanth <shashikanthvarma98@gmail.com>
2021-06-16 06:18:42 +0000

    Update insertionsort.py

--- insertionsort.py
+++ insertionsort.py
@@ -1,3 +1,6 @@
+#this is an implementaion of insertion sort
+#detail explanation at https://www.youtube.com/watch?v=Gq8lno2N0n0&t=
+
 def insertionsort(a):
     for i in range(1,len(a)):
         for j in range(i):
